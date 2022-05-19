39d3f55c1ceb147131690f51e6aa5aa358ea57a4
Giacomo Pasin <giacomo.pasin@gmail.com>
2022-05-19 15:58:55 +0200

    added procedure icon

--- src/components/ProcedureDetails.tsx
+++ src/components/ProcedureDetails.tsx
@@ -1,3 +1,4 @@
+import { Description } from "@mui/icons-material";
 import { List } from "@mui/material";
 import { FC, useState } from "react";
 import { BlurDetails } from "./BlurDetails";
@@ -21,7 +22,7 @@ export const ProcedureDetails: FC<IProcedureDetailsProps> = ({ showDetails, isLo
       <List disablePadding>
         <RequestButton
           text="Richedi Procedure"
-          icon={null}
+          icon={<Description />}
           handleRequest={handleLoadProcedure}
         />
         <BlurDetails>
